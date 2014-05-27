c3fc30c3f26d9fd5b4824817d2e7db81a4ab11db
Steve King <steve.king@thisisdare.com>
2014-05-27 16:13:09 +0100

    Adding Jekyll build task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,27 +4,13 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     sass: {
       dist: {
+        options : {
+          style : 'compressed',
+          sourcemap : true
+        },
         files: {
           'assets/css/style.css': 'assets/scss/style.scss'
         }
-      }
-    },
-    watch: {
-      sass : {
-        files: ['assets/scss/*.scss',
-                'assets/scss/*/*.scss'],
-        tasks: ['sass']
-      },
-      js : {
-        files: ['assets/js/*.js',
-              'assets/js/**/*.js',
-              '!assets/js/vendor/*.js',
-              '!assets/js/plugins/*.js'],
-        tasks: ['jshint']
-      },
-      images : {
-        files: ['assets/images/**/*'],
-        tasks: ['crunch']
       }
     },
     jshint: {
@@ -52,16 +38,52 @@ module.exports = function(grunt) {
           dest : 'assets/images'
         }]
       }
-    }
+    },
+    shell: {
+      jekyllBuild: {
+        command: 'jekyll build'
+      }
+    },
+    watch: {
+      sass : {
+        files: ['assets/scss/*.scss',
+                'assets/scss/*/*.scss'],
+        tasks: ['sass']
+      },
+      js : {
+        files: ['assets/js/*.js',
+              'assets/js/**/*.js',
+              '!assets/js/vendor/*.js',
+              '!assets/js/plugins/*.js'],
+        tasks: ['jshint']
+      },
+      imagemin : {
+        files: ['assets/images/**/*.{png,jpg,gif}'],
+        tasks: ['crunch']
+      },
+      shell : {
+        files: ['**/*.markdown',
+                '**/*.html',
+                'assets/**/*.{png,jpg,gif,css,js}',
+                '_config.yml',
+                '!_site/**/*'],
+        tasks: ['shell:jekyllBuild'],
+        options: {
+            interrupt: true,
+            atBegin: true
+        }
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
 
   // default tasks run in development
-  grunt.registerTask('default', ['sass', 'jshint']);
+  grunt.registerTask('default', ['sass', 'jshint', 'crunch', 'shell']);
   grunt.registerTask('crunch', ['imagemin']);
   
 };
