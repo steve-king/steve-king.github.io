04165009065755c4712df3bc26fe9bffedff65b4
Steve King <steve.king@thisisdare.com>
2014-05-28 13:09:37 +0100

    Fleshing out JS structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,51 @@
 jQuery(document).ready(function($){
-	$('.posts').packery({
-		itemSelector: '.post'
+
+	//var windowWidth = $(window).width();
+
+	_.extend(sk.config, {
+		// Set config vars in here
+		//prevWidth : windowWidth,
+		//currentWidth : windowWidth,
 	});
+
+	sk.init();
 });
+
+sk.init = function(){
+
+	sk.getDOMSelectors();
+
+	sk.onResize();
+	$(window).on('resize', _.debounce(function(){		
+		sk.onResize();
+	}, 200));
+
+	sk.postsLayout();
+};
+
+sk.getDOMSelectors = function(){
+	sk.dom = {};
+	sk.dom.$window = $(window);
+	sk.dom.$postsContainer = $('.posts-container');
+};
+
+sk.onResize = function(){
+	sk.config.prevWidth = (sk.config.currentWidth != undefined) ? sk.config.currentWidth : sk.dom.$window.width();
+	sk.config.prevHeight = (sk.config.currentHeight != undefined) ? sk.config.currentHeight : sk.dom.$window.height();
+
+	sk.config.currentWidth  = sk.dom.$window.width();
+	sk.config.currentHeight = sk.dom.$window.height();
+	
+	if( sk.packery !== undefined ){
+		sk.packery.layout();
+	}
+};
+
+sk.postsLayout = function(){
+	sk.packery = new Packery(sk.dom.$postsContainer[0], {
+		itemSelector: '.post',
+		isResizeBound : false
+	});
+};
+
+
